2b5a7313eed216a294487a2e180208b6fe4c244c
Fredele20 <victorisholaoladele@gmail.com>
2023-09-19 08:01:05 +0100

    > added unit test for products.sql.go file > added some helper functions in utils.go file to generate id, random name, random description, random price > added the testify package for unit test > added the github.com/oklog/ulid package for generating id

---
+++ utils/random.go
@@ -0,0 +1,46 @@
+package utils
+
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// RandomInt generate a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+// RandomString generates a random string of length n
+func RandomString(n int) string {
+	var sb strings.Builder
+	k := len(alphabet)
+
+	for i := 0; i < n; i++ {
+		c := alphabet[rand.Intn(k)]
+		sb.WriteByte(c)
+	}
+
+	return sb.String()
+}
+
+// RandomName generates a random owner name
+func RandomName() string {
+	return RandomString(6)
+}
+
+// RandomDescription generates a random description for a product
+func RandomDescription() string {
+	return RandomString(12)
+}
+
+// RandomAmount generates a random amount of money
+func RandomPrice() int64 {
+	return RandomInt(0, 1000)
+}
